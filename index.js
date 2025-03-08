50b8283eb10ab6713b6c4e63c9342aa06e1ca130
golap-raj <golapraj47@gmail.com>
2025-03-08 23:47:08 +0600

    add let var const

---
+++ index.js
@@ -0,0 +1,22 @@
+// amra ata kokhono used korbo nha ata old ekta quard
+// console.log(name)
+
+// var name = "golap";
+
+// amra nicher dui ta used korbo let & const
+
+// let diye diclar korle change kora jai
+
+
+// let name = "golap"
+// name = "raj"
+// console.log(name)
+
+// const diye diclar korle change kora jai nha
+
+// const name = "Golap";
+// name = "raj"
+// console.log(name)
+
+
+
